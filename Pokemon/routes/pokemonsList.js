94438c26194e71f969cba246022a449754e91784
QuaRang <dbduddnd1225@gmail.com>
2024-05-20 13:51:37 +0900

    #2 - 포켓몬 도감별 구분
    - 현재 포켓몬 도감을 선택해도 전국도감으로 정렬되어 도감 별로 포켓몬이 안나오는 문제 발생
    - 특정 지역에 해당하는 도감 번호를 정렬해 도감을 바꿀 때마다 재기능을 하도록 코드 수정

--- Pokemon/routes/pokemonsList.js
+++ Pokemon/routes/pokemonsList.js
@@ -5,17 +5,18 @@ const mongoose = require('mongoose')
 const Pokemon = require('../models/pokemon')
 
 
+
+
+
 router.get('/', (req, res, next) => {
     const page = parseInt(req.query.page) || 1 // 페이지 번호, 기본값은 1
     const region = req.query.region
-
     const type1 = req.query.types_1 || ''
     const type2 = req.query.types_2 || ''
     const query = req.query.query
     const perPage = 20 // 페이지당 아이템 수
 
-
-    let condition = { "dex.region": region} // 기본 쿼리
+    let condition = { "dex.region": region } // 기본 쿼리
     if (query !== '') {
         // 부분 일치를 검색하는 정규 표현식 생성
         const regex = new RegExp(query, 'i'); // 'i' 플래그는 대소문자 구분 없이 검색
@@ -26,16 +27,18 @@ router.get('/', (req, res, next) => {
     } else if (type1 !== '' || type2 !== '') {
         condition["base.types"] = { $in: [type1, type2] }; // 두 가지 타입 중 하나를 포함하는 조건 추가
     }
-
     Pokemon
-        .countDocuments() // 전체 문서 수를 가져옴
+        .countDocuments(condition) // 필터링된 문서 수를 가져옴
         .then(totalCount => {
-            Pokemon
-                .find(condition)
-                .sort({ _id: 1 })
-                .skip((page - 1) * perPage) // 스킵할 아이템 수 계산
-                .limit(perPage) // 한 페이지에 반환할 아이템 수 제한
-                .exec()
+            Pokemon.aggregate([
+                { $match: condition }, // 매칭 조건 적용
+                { $unwind: "$dex" }, // dex 배열의 각 요소를 분리
+                { $match: { "dex.region": region } }, // 특정 region에 해당하는 dex만 선택
+                { $sort: { "dex.num": 1 } }, // num으로 정렬
+                { $addFields: { totalCount: totalCount } }, // totalCount 필드에 총 갯수 할당
+                { $skip: (page - 1) * perPage }, // 스킵할 아이템 수 계산
+                { $limit: perPage }, // 한 페이지에 반환할 아이템 수 제한
+            ])
                 .then(docs => {
                     const totalPages = Math.ceil(totalCount / perPage) // 전체 페이지 수 계산
                     const response = {
@@ -45,56 +48,59 @@ router.get('/', (req, res, next) => {
                         per_page: perPage, // 페이지당 아이템 수
                         pokemon: docs.map(doc => {
                             return {
-                                _id : doc._id,
-                                color : doc.color,
-                                name : doc.name,
-                                base :{
-                                    types : doc.base.types,
-                                    image : doc.base.image
+                                _id: doc.dex.num,
+                                color: doc.color,
+                                name: doc.name,
+                                base: {
+                                    num:doc._id,
+                                    types: doc.base.types,
+                                    image: doc.base.image
                                 }
                             }
                         })
                     };
                     res.status(200).json({
-                        status : 200,
-                        data : response,
-                        message :  "정상적으로 포켓몬 정보를 조회했습니다."
+                        status: 200,
+                        data: response,
+                        message: "정상적으로 포켓몬 정보를 조회했습니다."
                     })
                 })
                 .catch(err => {
                     res.status(500).json({
-                        status : 500,
-                        data : {},
-                        message : "포켓몬 정보 요청이 실패했습니다."
+                        status: 500,
+                        data: {},
+                        message: "포켓몬 정보 요청이 실패했습니다."
                     })
                 })
         })
         .catch(err => {
             res.status(500).json({
-                status : 500,
-                data : {},
-                message : "포켓몬 정보 요청이 실패했습니다."
+                status: 500,
+                data: {},
+                message: "포켓몬 정보 요청이 실패했습니다."
             })
         })
 })
-router.delete("/",(req,res,next) => {
+
+router.delete("/", (req, res, next) => {
     Pokemon.deleteMany({})
-    .exec()
-    .then(result => {
-        if (result){
-            res.status(200).json({
-                status : 200,
-                data:{},
-                message : "정상적으로 포켓몬 정보를 삭제했습니다."
-            })
-        }
-    })
-    .catch(err => {
-        res.status(500).json({
-            status : 500,
-            data : {},
-            message : "포켓몬 삭제 요청이 실패했습니다."
+        .exec()
+        .then(result => {
+            if (result) {
+                res.status(200).json({
+                    status: 200,
+                    data: {},
+                    message: "정상적으로 포켓몬 정보를 삭제했습니다."
+                })
+            }
         })
-    }) 
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                data: {},
+                message: "포켓몬 삭제 요청이 실패했습니다."
+            })
+        })
 })
+
 module.exports = router
